de981610091ef11e811f9abc31aecb023686b011
Seungchul Shim <tlatmdcjf94@naver.com>
2024-11-12 16:21:53 +0900

    feat: add reactive elastic query service

---
+++ elastic-query-web-client-common/src/main/java/com/microservices/demo/elastic/query/web/client/common/exception/ElasticQueryWebClientException.java
@@ -0,0 +1,16 @@
+package com.microservices.demo.handler.exception;
+
+public class ElasticQueryWebClientException extends RuntimeException {
+
+    public ElasticQueryWebClientException() {
+        super();
+    }
+
+    public ElasticQueryWebClientException(String message) {
+        super(message);
+    }
+
+    public ElasticQueryWebClientException(String message, Throwable t) {
+        super(message, t);
+    }
+}
